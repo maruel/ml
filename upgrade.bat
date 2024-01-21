f047c979d8b917356f3edf004bc8bfe60b33f010
Marc-Antoine Ruel <marc-antoine@thebrowser.company>
2024-01-21 09:42:31 -0500

    Try to install less packages on Windows
    This was to remove issues with pyls but it didn't seem to help anyhow.

--- upgrade.bat
+++ upgrade.bat
@@ -11,17 +11,19 @@ python -m pip install --upgrade pip
 :: https://github.com/facebookresearch/xformers/issues/740#issuecomment-1780727152
 pip3 install --upgrade setuptools wheel
 
+echo Installing pytorch with nvidia/CUDA packages
+::pip3 install nvidia-pyindex
+::pip3 install nvidia-cuda-runtime-cu12
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
 echo Installing stable diffusion packages
 pip3 install --upgrade ^
     accelerate ^
     diffusers ^
     numpy ^
     tiktoken ^
-    torch ^
-    torchvision ^
     omegaconf ^
     transformers
-
 
 pip3 install --upgrade --no-dependencies xformers
 
@@ -60,21 +62,23 @@ pip3 install --upgrade ^
 :: If you have an Intel Arc video card. Untested. This version does work on Windows. (!!)
 :: pip3 install --upgrade intel-extension-for-pytorch==2.1.10 --extra-index-url https://pytorch-extension.intel.com/release-whl/stable/xpu/us/
 
-echo Installing nvidia/CUDA packages
+::echo Installing nvidia/CUDA packages
 :: TODO(maruel): Currently incompatible with jupyterlab 4.
-pip3 install --upgrade jupyterlab-nvdashboard
+::pip3 install --upgrade jupyterlab-nvdashboard
 
-
+: The whole thing doesn't seem to work well. I even got
+:: parso to state that it doesn't support python 3.11, which is already
+:: quite old. Surprisingly, removing these didn't remove the exception.
 :: failed to install: rope_completion, rope_rename
-echo Installing linters
-pip3 install --upgrade ^
-    autopep8 ^
-    mccabe ^
-    pycodestyle ^
-    pydocstyle ^
-    pyflakes ^
-    pylint ^
-    yapf
+::echo Installing linters
+::pip3 install --upgrade ^
+::    autopep8 ^
+::    mccabe ^
+::    pycodestyle ^
+::    pydocstyle ^
+::    pyflakes ^
+::    pylint ^
+::    yapf
 
 :: Manually upgrade dependencies that are known to have security issues.
 echo Security updates
